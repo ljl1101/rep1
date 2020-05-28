ad487b13df2832ff958c7d88b77ab5db0010f4ac
ljl1101 <63136264+ljl1101@users.noreply.github.com>
2020-05-28 19:53:18 +0800

    Update Hellow.java

--- testGit/src/main/java/Hellow.java
+++ testGit/src/main/java/Hellow.java
@@ -1,8 +1,6 @@
 public class Hellow {
     public static void main(String args[]){
-        int i=0;
-        int i1=1;
-   
+     
         String sa="fdf";
           String sa1="ffffffffffdf";
     }
